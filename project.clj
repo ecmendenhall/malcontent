953846baec9907c604fa76d6af8bdfbaba1da061
Connor Mendenhall <ecmendenhall@gmail.com>
2013-08-17 14:50:21 -0500

    First commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject malcontent "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :plugins [[speclj "2.5.0"]]
+  :test-paths ["spec"])
---
+++ spec/malcontent/core_spec.clj
@@ -0,0 +1,4 @@
+(ns malcontent.core-spec
+  (:require [speclj.core :refer :all]
+            [malcontent.core :refer :all]))
+
